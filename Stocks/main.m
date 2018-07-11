99d6e5311be099d787b147a7747ce699404a98ea
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-11 16:46:40 -0700

    This works but the displayed numbers are objects and not usable for math

--- Stocks/main.m
+++ Stocks/main.m
@@ -7,11 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRStockHolding.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        BNRStockHolding *Telus = [[BNRStockHolding alloc]init];
+        [Telus setPurchaseSharePrice:100];
+        [Telus setNumberOfShare:25];
+        [Telus setCurrentSharePrice:125];
+        
+        BNRStockHolding *Bell = [[BNRStockHolding alloc]init];
+        [Bell setPurchaseSharePrice:37];
+        [Bell setNumberOfShare:87];
+        [Bell setCurrentSharePrice:87];
+        
+        BNRStockHolding *Rogers = [[BNRStockHolding alloc]init];
+        [Rogers setPurchaseSharePrice:224];
+        [Rogers setNumberOfShare:10];
+        [Rogers setCurrentSharePrice:276];
+        
+        //        In main(), fill an array with three instances of BNRStockHolding. Then iterate through the array printing out the value of each.
+        
+        
+        NSArray *allStocks = @[Telus, Bell, Rogers];
+        int count = 0;
+        for (NSNumber *w in allStocks)
+        {
+            NSLog(@"%f, %f, %f", [allStocks[count] purchaseSharePrice],[allStocks[count] numberOfShare], [allStocks[count] currentSharePrice]);
+            count = count +1;
+        }
+        
     }
+    
     return 0;
 }
+
